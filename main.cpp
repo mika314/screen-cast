8d48e4a2054908491da59dec8ae94bf5ee1d6531
Mika Pi <mika@mika.global>
2024-11-16 19:23:46 -0800

    Package the HTTP and WebSocket server in one executable

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include <boost/beast.hpp>
 #include <chrono>
 #include <fcntl.h>
+#include <fstream>
 #include <iostream>
 #include <log/log.hpp>
 #include <sys/ipc.h>
@@ -21,22 +22,23 @@ extern "C" {
 
 using tcp = boost::asio::ip::tcp;
 namespace websocket = boost::beast::websocket;
+namespace http = boost::beast::http;
 
 class WebSocketSession : public std::enable_shared_from_this<WebSocketSession>
 {
 public:
   WebSocketSession(tcp::socket socket) : ws_(std::move(socket))
   {
-    // Initialize FFmpeg encoder
+    LOG("Initialize FFmpeg encoder");
     init_encoder();
   }
 
-  void run()
+  void run(http::request<http::string_body> req)
   {
-    // Accept the WebSocket handshake
-    ws_.accept();
+    LOG("Accept the WebSocket handshake");
+    ws_.accept(req);
 
-    // Start sending frames
+    LOG("Start sending frames");
     start_sending_frames();
   }
 
@@ -319,69 +321,163 @@ private:
   const int y_ = 0;
 };
 
-auto doAccept(boost::asio::io_context &ioc, std::shared_ptr<boost::asio::ip::tcp::acceptor> acceptor)
-  -> void;
+// Forward declaration
+void do_accept(tcp::acceptor &acceptor);
 
-void do_listen(boost::asio::io_context &ioc, tcp::endpoint endpoint)
+// Session class to handle both HTTP and WebSocket sessions
+class Session : public std::enable_shared_from_this<Session>
 {
-  auto acceptor = std::make_shared<tcp::acceptor>(ioc);
-  boost::system::error_code ec;
+public:
+  Session(tcp::socket socket) : socket_(std::move(socket)), strand_(socket_.get_executor()) {}
 
-  acceptor->open(endpoint.protocol(), ec);
-  if (ec)
+  void run()
   {
-    LOG("Failed to open acceptor:", ec.message());
-    return;
+    LOG("Read a request");
+    do_read();
   }
 
-  acceptor->set_option(boost::asio::socket_base::reuse_address(true), ec);
-  if (ec)
+private:
+  tcp::socket socket_;
+  boost::asio::strand<boost::asio::any_io_executor> strand_;
+  boost::beast::flat_buffer buffer_;
+  http::request<http::string_body> req_;
+
+  void do_read()
   {
-    LOG("Failed to set reuse_address:", ec.message());
-    return;
+    auto self = shared_from_this();
+    http::async_read(socket_,
+                     buffer_,
+                     req_,
+                     boost::asio::bind_executor(
+                       strand_, [self](boost::system::error_code ec, std::size_t bytes_transferred) {
+                         boost::ignore_unused(bytes_transferred);
+                         if (!ec)
+                           self->handle_request();
+                       }));
   }
 
-  acceptor->bind(endpoint, ec);
-  if (ec)
+  void handle_request()
   {
-    LOG("Failed to bind:", ec.message());
-    return;
+    LOG("Handle WebSocket handshake");
+    if (websocket::is_upgrade(req_))
+    {
+      LOG("Create WebSocket session and transfer ownership of the socket");
+      std::make_shared<WebSocketSession>(std::move(socket_))->run(std::move(req_));
+      return;
+    }
+
+    LOG("Handle HTTP request");
+    handle_http_request();
   }
 
-  acceptor->listen(boost::asio::socket_base::max_listen_connections, ec);
-  if (ec)
+  void handle_http_request()
   {
-    LOG("Failed to listen:", ec.message());
-    return;
+    LOG("Build the path to the requested file");
+    std::string path = req_.target().to_string();
+    if (path == "/")
+      path = "/index.html";
+
+    std::string full_path = "." + path; // Assuming files are in the current directory
+
+    LOG("Open the file", full_path);
+    std::ifstream file(full_path, std::ios::in | std::ios::binary);
+    if (!file)
+    {
+      LOG("File not found, send 404 response");
+      send_not_found_response(path);
+      return;
+    }
+
+    LOG("Read the file content");
+    std::stringstream ss;
+    ss << file.rdbuf();
+    std::string content = ss.str();
+
+    LOG("Determine the content type");
+    std::string content_type;
+    if (path.ends_with(".html"))
+      content_type = "text/html";
+    else if (path.ends_with(".js"))
+      content_type = "application/javascript";
+    else if (path.ends_with(".css"))
+      content_type = "text/css";
+    else
+      content_type = "application/octet-stream";
+
+    LOG("Build the response");
+    // Use a shared_ptr to extend the lifetime of res
+    auto res = std::make_shared<http::response<http::string_body>>(http::status::ok, req_.version());
+    res->set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res->set(http::field::content_type, content_type);
+    res->keep_alive(req_.keep_alive());
+    res->body() = content;
+    res->prepare_payload();
+
+    LOG("Send the response", content);
+    auto self = shared_from_this();
+    http::async_write(
+      socket_,
+      *res,
+      boost::asio::bind_executor(strand_, [self, res](boost::system::error_code ec, std::size_t) {
+        LOG("response sent");
+        self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+      }));
   }
 
-  doAccept(ioc, acceptor);
-}
+  void send_not_found_response(const std::string &target)
+  {
+    LOG("Build the 404 response");
+    auto res =
+      std::make_shared<http::response<http::string_body>>(http::status::not_found, req_.version());
+    res->set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res->set(http::field::content_type, "text/html");
+    res->keep_alive(req_.keep_alive());
+    res->body() = "The resource '" + target + "' was not found.";
+    res->prepare_payload();
 
-auto doAccept(boost::asio::io_context &ioc, std::shared_ptr<boost::asio::ip::tcp::acceptor> acceptor)
-  -> void
+    LOG("Send the response");
+    auto self = shared_from_this();
+    http::async_write(
+      socket_,
+      *res,
+      boost::asio::bind_executor(strand_, [self, res](boost::system::error_code ec, std::size_t) {
+        self->socket_.shutdown(tcp::socket::shutdown_send, ec);
+      }));
+  }
+};
+
+// Start accepting connections
+void do_accept(tcp::acceptor &acceptor)
 {
-  acceptor->async_accept([acceptor, &ioc](boost::system::error_code ec, tcp::socket socket) {
+  acceptor.async_accept([&](boost::system::error_code ec, tcp::socket socket) {
     if (!ec)
     {
-      std::make_shared<WebSocketSession>(std::move(socket))->run();
+      std::make_shared<Session>(std::move(socket))->run();
     }
     else
     {
       LOG("Accept failed:", ec.message());
     }
-    doAccept(ioc, acceptor);
+    do_accept(acceptor);
   });
 }
 
 int main()
 {
-  boost::asio::io_context ioc;
-  tcp::endpoint endpoint(tcp::v4(), 8090);
+  try
+  {
+    boost::asio::io_context ioc{1};
+    tcp::endpoint endpoint{tcp::v4(), 8090};
 
-  do_listen(ioc, endpoint);
+    tcp::acceptor acceptor{ioc, endpoint};
+    do_accept(acceptor);
 
-  ioc.run();
+    ioc.run();
+  }
+  catch (const std::exception &e)
+  {
+    LOG("Error:", e.what());
+  }
 
   return 0;
 }
--- readme.md
+++ readme.md
@@ -1,10 +1,17 @@
+# Screen Cast
+
 Connect your Oculus Quest to your computer with a USB cable.
 
 Run in the console:
 
 ```
-adb reverse tcp:8090 tcp:809
+adb reverse tcp:8090 tcp:8090
 ```
 
-open in the browser https://mika.global/screen-cast/index.html
+Then, start the screen-cast server:
 
+```
+./screen-cast
+```
+
+Open in the Oculus Quest browser: `http://localhost:8090`
